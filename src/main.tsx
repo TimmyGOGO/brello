eef10cb94703d1d809113bbdbe54987ceac08bca
Artem Ivanov <temich941@mail.ru>
2024-10-20 23:28:10 +0300

    Fixed lint and prettier errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 
 import { MantineProvider } from "@mantine/core";
+
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/dropzone/styles.css";
@@ -9,10 +9,11 @@ import "@mantine/notifications/styles.css";
 import "@mantine/spotlight/styles.css";
 import "@mantine/tiptap/styles.css";
 
+import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <MantineProvider>
     <App />
-  </MantineProvider>
+  </MantineProvider>,
 );
