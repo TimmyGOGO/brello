cc524500945181376cdb46a3917dc4668093e40f
Artem Ivanov <temich941@mail.ru>
2024-11-03 19:33:02 +0300

    Add basic layout for kanban cards

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 
 import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
-import "@mantine/dropzone/styles.css";
-import "@mantine/notifications/styles.css";
-import "@mantine/spotlight/styles.css";
-import "@mantine/tiptap/styles.css";
 
+import { Application } from "./application.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <MantineProvider>
-    <App />
+    <Application />
   </MantineProvider>
 );
